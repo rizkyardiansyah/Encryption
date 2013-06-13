0c0cab48cf7fcce17a6e3ed68e21c5df9386e8c4
Rizky Ardiansyah <rizky.ardiansyah@ui.ac.id>
2013-06-13 19:01:59 +0700

    Create Encryption.java

---
+++ Encryption.java
@@ -0,0 +1,38 @@
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author ardi
+ * @email: rizky.ardiansyah4891@gmail.com
+ */
+public class Encryption {
+
+  public static void main(String[] bluesLegend){
+		// put your input text here!
+		String inputText = "Eric Clapton";
+		System.out.println(encrypt(inputText));
+	}
+	
+    public static String encrypt(String plainText) {
+        String cipherText = null;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] hash = md.digest(plainText.getBytes("UTF-8")); //converting byte array to Hexadecimal 
+            StringBuilder sb = new StringBuilder(2 * hash.length);
+
+            for (byte b : hash) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+
+            cipherText = sb.toString();
+        } 
+		catch (UnsupportedEncodingException ex) {} 
+		catch (NoSuchAlgorithmException ex) {}
+
+        return cipherText;
+    }
+}
